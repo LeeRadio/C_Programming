4f8d9913d97ca1d479987687d6c795757625e574
LeeRadio <acorn0987@naver.com>
2020-07-11 23:54:57 +0900

    변수 덧셈뺄샘나누기
    변수 선언이후 설정된 숫자를
    덧셈 뺄셈 나누기

---
+++ C_program2-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int a,b;
+
+a = 5;
+b = 3;
+int main(void)
+{
+	printf("\변수 A에 저장된 숫자는 %d 입니다!\n", a);
+	printf("\n변수 B에 저장된 숫자는 %d 입니다.\n", b);
+	printf("\n변수 A와 B를 덧셈하면 %d 입니다.\n", a+b);
+	printf("\n변수 A와 B를 곱셈하면 %d 입니다.\n", a*b);
+	printf("\n변수 A와 B를 뺄셈하면 %d 입니다.\n", a-b);
+	printf("\n변수 A와 B를 나누기하면 %d 입니다.\n", a/b);
+	
+}
